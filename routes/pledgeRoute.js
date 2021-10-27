f8ea8d7006d064d4661e5957068d64f67e59faaf
izeberg <skizeza666@gmail.com>
2021-10-28 02:31:12 +0700

    edit pledge route get-by-project-id

--- routes/pledgeRoute.js
+++ routes/pledgeRoute.js
@@ -10,11 +10,7 @@ const {
 const router = express.Router();
 
 router.get("/get-all", passport.authenticate("jwt-admin", { session: false }), getAllPledge);
-router.get(
-    "/get-by-project-id/:projectId",
-    passport.authenticate("jwt-user", { session: false }),
-    getPledgeByProjectId
-);
+router.get("/get-by-project-id/:projectId", getPledgeByProjectId);
 router.get("/get-by-user-id", passport.authenticate("jwt-user", { session: false }), getPledgeByUserId);
 router.post("/create", passport.authenticate("jwt-user", { session: false }), createPledge);
 router.put("/update-status", passport.authenticate("jwt-admin", { session: false }), updatePledgeStatus);
